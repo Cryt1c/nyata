fc58bd0359461a8732d2fb5869b94d55d44207ff
Cryt1c <david@peherstorfer.info>
2023-05-21 11:00:00 +0200

    Remove console.log

--- src/components/Board/List/utils/list.ts
+++ src/components/Board/List/utils/list.ts
@@ -2,7 +2,6 @@ export const getHighestIdonBoard = (board) => {
   const highestIdsOnEachList = board.map((list) => {
     return list.reduce((acc, item) => {
       if (item.id > acc) {
-        console.log(item.id);
         return item.id;
       }
       return acc;
